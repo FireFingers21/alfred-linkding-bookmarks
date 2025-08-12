123fd51ba8665a692ee6898dd1d8bddf290d30f5
firefingers21 <online+github@alexhuisman.ca>
2025-08-12 14:22:53 -0700

    Rework favicon download process w/ improved parallelization

--- Workflow/reload.sh
+++ Workflow/reload.sh
@@ -9,17 +9,13 @@ case "${http_code}" in
 		readonly favicon_folder="${alfred_workflow_data}/favicons"
 
 		mkdir -p "${alfred_workflow_data}"
-		readonly oldBookmarks=$(< ${bookmarks_file} | md5)
+		readonly oldFavicons=($(jq -rs '.[].results | map(.favicon_url) | unique | .[]' ${bookmarks_file}))
 		echo -nE "${bookmarks%$'\n'*}" > "${bookmarks_file}"
-		readonly newBookmarks=$(< ${bookmarks_file} | md5)
+		readonly newFavicons=($(jq -rs '.[].results | map(.favicon_url) | unique | .[]' ${bookmarks_file}))
 
-		if [[ "${useFavicons}" -eq 1 && "${oldBookmarks}" != "${newBookmarks}" ]]; then
+		if [[ "${useFavicons}" -eq 1 && ("${oldFavicons}" != "${newFavicons}" || ! -d "${favicon_folder}") ]]; then
 		    mkdir -p "${favicon_folder}"
-			for url in $(jq -rs '.[].results | map(.favicon_url) | .[]' ${bookmarks_file} | sort | uniq); do
-			    filename=$(basename "$url")
-				curl -s -o "${favicon_folder}/$filename" "$url" &
-				[[ $(jobs | wc -l) -eq 15 ]] && wait
-			done
+			curl -s --compressed --parallel --output-dir "${favicon_folder}" --remote-name-all -L "${newFavicons[@]}"
 			find "${favicon_folder}" -type f -maxdepth 1 ! -newer "${bookmarks_file}" -delete
 		fi
 
