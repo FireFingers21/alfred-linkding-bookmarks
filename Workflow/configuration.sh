f0c83632b4f328903f0c59a659bae10d07fe53ec
firefingers21 <online+github@alexhuisman.ca>
2025-10-04 19:42:25 -0700

    Simplify :: actions

--- Workflow/configuration.sh
+++ Workflow/configuration.sh
@@ -2,8 +2,6 @@
 
 # Get lastest cache timestamp
 readonly bookmarks_file="${alfred_workflow_data}/bookmarks.json"
-readonly favicons_file="${alfred_workflow_data}/favicons"
-
 readonly lastUpdated=$(date -r "${bookmarks_file}" +"%A, %B %d %Y at %I:%M%p" || printf "Never")
 
 cat << EOB
@@ -14,35 +12,19 @@ cat << EOB
 		"variables": { "pref_id": "reload" }
 	},
 	{
-		"title": "New Bookmark",
-		"variables": { "pref_id": "new" },
-		"mods": {
-			"cmd": {
-				"subtitle": "Open in secondary browser",
-				"variables": {
-					"bSecondary": true,
-					"pref_id": "new"
-				}
-			}
-		}
-	},
-	{
 		"title": "Open linkding",
 		"variables": { "pref_id": "open" },
 		"mods": {
 			"cmd": {
-				"subtitle": "Open in secondary browser",
-				"variables": {
-					"bSecondary": true,
-					"pref_id": "open"
-				}
+				"subtitle": "⌘↩ Open in secondary browser",
+				"variables": { "bSecondary": true, "pref_id": "open" }
 			}
 		}
 	},
 	{
-		"title": "Browser Settings",
-		"subtitle": "Select the default browsers for ${alfred_workflow_name}",
-		"variables": { "pref_id": "browser" }
+		"title": "Configure Workflow...",
+		"subtitle": "Open the configuration window for ${alfred_workflow_name}",
+		"variables": { "pref_id": "configure" }
 	},
 ]}
 EOB
